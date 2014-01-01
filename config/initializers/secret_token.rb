1cb12ebf06b1856f9c960c9f01f9f2ce7f5d460b
ahnbizcad <ahnbizcad@gmail.com>
2014-01-01 07:03:04 -0800

    initial commit - blank scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Charactertypes::Application.config.secret_key_base = '2ca6ac0e136524602101608f8e8de31fa1347226c0756140da00707dc86f60d0e0b6778f0d65702485e927892ee64b70a6a3d64f39730fcc4ca3e63d0b4530f2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Charactertypes::Application.config.session_store :cookie_store, key: '_charactertypes_session'
